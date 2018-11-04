35df1202f05a0cb9ccdd077836aebbbca5dabc2c
David Dunson <david.dunson@gsdlogic.com>
2018-11-04 06:50:53 -0600

    Added empty ASP.NET Core project.

---
+++ IdentityServer/IdentityServerQuickStart/Program.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="GSD Logic">
+//   Copyright © 2018 GSD Logic. All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace IdentityServerQuickStart
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class Program
+    {
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+    }
+}
