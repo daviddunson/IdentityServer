03a82fa87e633993207d50a3373440e5405fe536
David Dunson <david.dunson@gsdlogic.com>
2018-11-04 07:09:54 -0600

    Completed setup and overview.

--- IdentityServer/IdentityServerQuickStart/Startup.cs
+++ IdentityServer/IdentityServerQuickStart/Startup.cs
@@ -8,7 +8,6 @@ namespace IdentityServerQuickStart
 {
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.DependencyInjection;
 
     public class Startup
@@ -21,13 +20,15 @@ namespace IdentityServerQuickStart
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async context => { await context.Response.WriteAsync("Hello World!"); });
+            app.UseIdentityServer();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # IdentityServer
 Sample code for ASP.NET Core and IdentityServer4.
+
+## Setup and Overview
+
+http://docs.identityserver.io/en/release/quickstarts/0_overview.html
