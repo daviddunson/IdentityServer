6a90aa6d8a49f50bfb475bf29fea8576d5a5da53
David Dunson <david.dunson@gsdlogic.com>
2018-11-04 08:33:58 -0600

    Added client.

--- IdentityServer/ApiQuickStart/Startup.cs
+++ IdentityServer/ApiQuickStart/Startup.cs
@@ -15,7 +15,7 @@ namespace ApiQuickStart
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseMvc();
+            app.UseAuthentication();
             app.UseMvc();
         }
 
