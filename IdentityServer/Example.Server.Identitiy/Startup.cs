c448e1eb6ab599db531792ca73f8dad917839986
David Dunson <david.dunson@gsdlogic.com>
2020-05-26 04:00:18 -0500

    Added user secrets.

--- IdentityServer/Example.Server.Identitiy/Startup.cs
+++ IdentityServer/Example.Server.Identitiy/Startup.cs
@@ -1,18 +1,34 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Startup.cs" company="GSD Logic">
+// <copyright file="Program.cs" company="GSD Logic">
 //   Copyright © 2019 GSD Logic. All Rights Reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace Example.Server.Identity
 {
+    using System;
     using IdentityServer4;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
 
     public class Startup
     {
+        /// <summary>
+        /// The application configuration.
+        /// </summary>
+        private readonly IConfiguration configuration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup" /> class.
+        /// </summary>
+        /// <param name="configuration">The application configuration.</param>
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -43,8 +59,14 @@ namespace Example.Server.Identity
                 .AddGoogle("Google", options =>
                 {
                     options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    options.ClientId = "434483408261-55tc8n0cs4ff1fe21ea8df2o443v2iuc.apps.googleusercontent.com";
-                    options.ClientSecret = "3gcoTrEDPPJ0ukn_aYYT6PWo";
+                    options.ClientId = this.configuration["Google:ClientId"];
+                    options.ClientSecret = this.configuration["Google:ClientSecret"];
+                })
+                .AddFacebook("Facebook", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.AppId = this.configuration["Facebook:AppId"];
+                    options.AppSecret = this.configuration["Facebook:AppSecret"];
                 });
         }
     }
